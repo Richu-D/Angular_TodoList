581be514d4293e5cb2fed5f53f282fe1c9f050bb
Richu D <richu.developer@gmail.com>
2023-03-31 16:07:44 +0530

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
+  text = "";
 
+  todoList:Task[] =[]
+  ngOnInit(): void {
+     let todoList:Task[] = JSON.parse(localStorage.getItem("todoList")!) 
+     if(todoList){
+      this.todoList = todoList;
+     }
+  }
+  ngOnDestroy() {
+    localStorage.setItem('todoList', JSON.stringify(this.todoList));
+  }
+  addTodo(){
+    if(this.text.trim()){
+      let newTodo:Task = {
+        id:Math.floor(Math.random()*100000000),
+        task:this.text,
+        isFinished:false
+      }
+      this.todoList.push(newTodo)
+      this.text = ""
+    }
+    this.ngOnDestroy()
+  }
+  deleteTodo(todoId:number){
+    this.todoList = this.todoList.filter((todo)=>{
+      if(todo.id===todoId){
+        return false;
+      }
+      return true;
+    })
+    this.ngOnDestroy()
+  }
+  editTodo({todoId,task}:{todoId:number,task:string}){
+   this.todoList = this.todoList.filter(todo=>{
+    if(todo.id === todoId){
+      todo.task = task;
+    }
+    return todo;
+   })
+   this.ngOnDestroy()
+  }
+  toggleFinish(todoId:number){
+    this.todoList.map(todo=>{
+      if(todo.id === todoId){
+        todo.isFinished = !todo.isFinished;
+      }
+      return todo;
+    })
+    this.ngOnDestroy()
+  }
+  removeAllFinishedTodo(){
+    this.todoList = this.todoList.filter(todo=>{
+      if(todo.isFinished==true){
+        return false;
+      }
+      return true;
+    })
+    this.ngOnDestroy()
+  }
+  leftTodo(){
+    let nonFinished = this.todoList.reduce((count:number,todo:Task)=>{
+      if(!todo.isFinished){
+        return count = count + 1
+      }
+      return count;
+    },0)
+    if(nonFinished){
+      return `${nonFinished} item left`
+    }
+    return `Nothing is left to finish`
+  }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Input, Output } from '@angular/core';
+import { Task } from '../task.model';
 
 @Component({
   selector: 'app-todo',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
+@Input() todo!:Task;
+@Output() toggleFinish = new EventEmitter()
+@Output() deleteTodo = new EventEmitter()
+@Output() editTodo = new EventEmitter()
+editing:boolean = false
+constructor(){}
 
+onCheckboxChange(todoId:number) {
+ this.toggleFinish.emit(todoId)
+}
+
+delete(todoId:number){
+  this.deleteTodo.emit(todoId)
+}
+edit(todoId:number,task:string){
+  this.editTodo.emit({todoId,task})
+  this.editing = false;
+}
 }
